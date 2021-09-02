6bdd6db10b4aba3900069529231ee33e8484f3ac
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-09-02 00:01:00 -0600

    The monitoring of code changes was modified

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -5,7 +5,12 @@ import logger from './logger';
 export default function (app: Application): void {
   mongoose.connect(
     app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true
+    }
   ).catch(err => {
     logger.error(err);
     process.exit(1);
